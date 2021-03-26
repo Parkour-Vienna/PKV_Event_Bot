025b05a024a608931e4a3bfeb76ab10e66c139da
Swarsele <32304731+Swarsele@users.noreply.github.com>
2021-03-27 00:37:31 +0100

    added list of people who have voted

--- vote_remind.py
+++ vote_remind.py
@@ -3,6 +3,7 @@
 import requests
 import logging
 import datetime
+from helper import open_file
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
@@ -12,12 +13,14 @@ if __name__ == '__main__':
 
     logging.info('checking day')
     if todayday == 2:
+        votes_tn = open_file("tn.train")
         message = "*Voting for Tuesdayness will close in 2 hours! \nMake sure to get your vote in with /vote TN \[" \
-                  "SPOT]! "
+                  "SPOT]! \nThe following people have voted already: " + " ".join([item['who'] for item in votes_tn])
 
     if todayday == 6:
+        votes_fm = open_file("fm.train")
         message = "*Voting for Forum Meeting will close in 2 hours!* \nMake sure to get your vote in with /vote FM \[" \
-                  "SPOT]! "
+                  "SPOT]! \n\nThe following people have voted already:\n   " + " ".join([item['who'] for item in votes_fm])
 
     if todayday == 2 or todayday == 6:
         send = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + \
