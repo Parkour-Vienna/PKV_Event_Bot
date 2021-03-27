5f30b40b021266ca0265d2eb2cbe1e21aa275e80
Swarsele <32304731+Swarsele@users.noreply.github.com>
2021-03-27 04:35:17 +0100

    fixed stupid mistake

--- settings.py
+++ settings.py
@@ -1,14 +1,14 @@
 api_settings = {
-    forum_url: 'http://beta.parkourvienna.at',
-    api_name: 'Spotbot',
-    api_key: '2d6a1397fafdb361d9f4e3236d14e8cb3c1418de93883cd53cf7ad34ffe932e1'
+    'forum_url': 'http://beta.parkourvienna.at',
+    'api_name': 'Spotbot',
+    'api_key': '2d6a1397fafdb361d9f4e3236d14e8cb3c1418de93883cd53cf7ad34ffe932e1'
 }
 
 bot_settings = {
-    bot_token: '1634462832:AAEUuVYbbfQ47VrcximEFkMlBp9HF1a9xog',
-    bot_chatID: '-550704351'
+    'bot_token': '1634462832:AAEUuVYbbfQ47VrcximEFkMlBp9HF1a9xog',
+    'bot_chatID': '-550704351'
 }
 
 route_settings = {
-    text_dest: "telegram"
+    'text_dest': "telegram"
 }
